f0769312cf4bf59dc496c1366cdf0ab1b82df590
Ronzo07 <106911241+Ronzo07@users.noreply.github.com>
2024-04-12 05:29:50 +0000

    Refactor Host1.py and Host2.py in Phase2/C directory

--- Phase2/C/Host1.py
+++ Phase2/C/Host1.py
@@ -1,22 +1,22 @@
 from socket import *
 from threading import Thread
 
-hostName = '127.0.0.2'
+hostName = '127.0.0.2' 
 hostPort = 12000
-hostSocket = socket(AF_INET,SOCK_DGRAM)
-hostSocket.bind(('localhost',hostPort))
-hostSocket.settimeout(0.2)
-print("host is ready")
+hostSocket = socket(AF_INET, SOCK_DGRAM)
+hostSocket.bind(('localhost', hostPort))
+print("Host is ready")
 username1 = input("Register your contact's name please: ")
 
-def Recieve():
+def Receive():
     while True:
         try:
             message, addr = hostSocket.recvfrom(12000)
-            hostSocket.sendto("ACK1".encode(),(hostName,hostPort)) # Maybe here there is an issue
-            print(message.decode())
-        except:
-            pass
+            if message.decode() != "ACK":  # Don't print ACK messages
+                print(message.decode())
+            hostSocket.sendto("ACK".encode(), addr)  # Send ACK for all received messages
+        except Exception as e:
+            print("Error:", e)
 
 def Send():
     while True:
@@ -25,16 +25,15 @@ def Send():
             break
         acknowledged = False
         try:
-            while not acknowledged :
-                try:
-                    ACK, addr = hostSocket.recvfrom(12000)
-                    acknowledged  = True
-                except:
-                    hostSocket.sendto((username1 + ": " +message).encode(),(hostName,hostPort))
-        except:
-            pass
+            while not acknowledged:
+                hostSocket.sendto((username1 + ": " + message).encode(), (hostName, hostPort))
+                ACK, addr = hostSocket.recvfrom(12000)  # Wait for ACK
+                if ACK.decode() == "ACK":
+                    acknowledged = True
+        except Exception as e:
+            print("Error:", e)
 
-t1 = Thread(target=Recieve)
+t1 = Thread(target=Receive)
 t2 = Thread(target=Send)
 
 t1.start()
