f0c55b78fc47668826d3425b2127aaf809e9693c
Ronzo07 <106911241+Ronzo07@users.noreply.github.com>
2024-04-22 19:09:42 +0000

    Add comments, update readme

--- Phase4/Host1.py
+++ Phase4/Host1.py
@@ -5,6 +5,7 @@ import socket
 import sys
 import os
 
+# Host IPs
 localHost = '127.0.0.1'
 destHost = '127.0.0.2'
 
@@ -12,6 +13,7 @@ chatPortSend = 12000
 chatPortRecv = 12001
 filePort = 12002
 
+# Socket setup, first for sending messages and receiving ACK, second for receiving messages and sending ACK
 send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 recv_socket.bind((localHost, chatPortRecv))
@@ -21,7 +23,7 @@ username = simpledialog.askstring("Username", "Register your contact's name plea
 
 nbFile = '1'
 cntFile = 0
-ack_num = 0
+ack_num = 0 # Sequence number for the messages
 
 
 # Networking functions
@@ -60,7 +62,7 @@ def fileSend(fileName):
 
 def Receive(update_chat_window):
     """ Function to receive messages from the sender"""
-    seq_num = 0
+    seq_num = 0 # Sequence number for the messages expected to be received
     while True:
         try:
             message, addr = recv_socket.recvfrom(1024)
@@ -76,6 +78,8 @@ def Receive(update_chat_window):
                 else:
                     update_chat_window(msg_content)
             else:
+                # I always send an ACK, even if the message is out of order 
+                # so the sender knows it was received and can move on
                 recv_socket.sendto(f"ACK:{msg_seq_num}".encode(), addr)
                 print(f"Received out of order: {msg_content}")
 
@@ -88,17 +92,19 @@ def Send(message):
         send_socket.sendto((username + " has left the chat room").encode(), (destHost, chatPortSend))
         sys.exit()        
 
-    CHUNK_SIZE = 1024
+    CHUNK_SIZE = 1024 # Chunk size for the messages
     message_chunks = [message[i:i+CHUNK_SIZE] for i in range(0, len(message), CHUNK_SIZE)]
 
     for chunk in message_chunks:
         acknowledged = False
+        # Keep sending the message until an ACK is received
         while not acknowledged:
             send_socket.settimeout(5)  # Timeout after 5 seconds
             send_socket.sendto(f"{ack_num}:{username}: {chunk}".encode(), (destHost, chatPortSend))
             try:
                 ack_msg, addr = send_socket.recvfrom(1024)  # Wait for ACK
                 ack_seq_num = ack_msg.decode().split(':')[1]
+                # If the ACK sequence number matches the message sequence number, the message was received
                 if int(ack_seq_num) == ack_num:
                     acknowledged = True
                     ack_num += 1
@@ -113,19 +119,19 @@ class ChatApp:
     def __init__(self, master):
         self.master = master
         master.title("UDP/TCP Chat App1")
-
+        # GUI elements for the chat window and message entry
         self.text_area = tk.Text(master, state='disabled', height=15, width=50)
         self.text_area.pack(padx=20, pady=10)
-
+        # Entry field for the message to be sent
         self.msg_entry = tk.Entry(master, width=40)
         self.msg_entry.pack(padx=20, pady=10)
-
+        # Buttons for sending messages and attaching files
         self.send_button = tk.Button(master, text="Send", command=self.send_message)
         self.send_button.pack(side=tk.LEFT, padx=(20, 10), pady=10)
-
+        # Button to attach a file
         self.file_button = tk.Button(master, text="Attach File", command=self.attach_file)
         self.file_button.pack(side=tk.RIGHT, padx=(10, 20), pady=10)
-
+        # Start the receive thread
         self.recv_thread = Thread(target=self.receive)
         self.recv_thread.daemon = True
         self.recv_thread.start()
@@ -155,15 +161,18 @@ class ChatApp:
             print("File sent!")            
 
     def update_chat_window(self, message):
+        # Function to update the chat window with the messages
         self.text_area.config(state='normal')
         self.text_area.insert(tk.END, message + "\n")
         self.text_area.config(state='disabled')
         self.text_area.see(tk.END)
 
     def receive(self):
+        # Function to receive messages from the sender
         Receive(self.update_chat_window)
 
     def on_closing(self):
+        # Function to handle the closing of the chat window
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
             send_socket.sendto((username + " has left the chat room").encode(), (destHost, chatPortSend))
             self.master.destroy()
--- README.md
+++ README.md
@@ -1,11 +1,38 @@
 # EECE350-Chatting-App
-This is a Network chatting application developed for the EECE350 course at the American University of Beirut. The application allows two users to communicate with each other over a network connection. UDP was used to send and receiv messages after adding some reliability on top of it, while TCP was used in order to send any type of files. Users can send text messages and files to each other using the graphical user interface (GUI).
+This is a Network chatting application developed for the EECE350 course. The application allows two users to communicate with each other over a network connection. UDP was used to send and receiev messages after adding some reliability on top of it, while TCP was used in order to send all type of files.
+
+# Simple test of the application:
+Add network traffic
+```bash
+sudo tc qdisc add dev lo root netem delay 100ms 1000ms distribution normal loss 10% 25% reorder 25% 50% (delay, loss and reorder)
+```
+Run the following commands in two different terminals to start the chatting application:
+```bash
+cd Phase4
+python Host1.py
+```
+```bash
+cd Phase4
+python Host2.py
+```
+Remove network traffic
+```bash
+sudo tc qdisc del dev lo root
+```
+You first need to enter your name, then you can start sending messages and files. 
+
+# Specifications:
+Python 3.10 is required to run the application and corrsponding library are listed below.
+To install the required libraries, run the following command:
+```
+pip install socket tkinter threading os time tk sys
+```
 
 # Running the Application:
 To run the chatting application, follow these steps:
 
 1. Clone or download the project repository to your local machine.
-2. Navigate to the directory containing the project files (Phase4 contains the final version with GUI and Phase3 contains the terminal version). (cd Phase4 OR cd Phase3)
+2. Navigate to the directory containing the project files (Phase4 contains the final version with GUI and Phase3 contains the terminal version).:
 3. Open two terminal window.
 4. Run the following command to execute the Python script:
 python Host1.py
@@ -14,14 +41,14 @@ python Host2.py
 
 This commands will start the application, and the GUI will be displayed on your screen.
 
-# Using the Application (Phase4):
+# Using the Application GUI (Phase4):
 Once the application is running, you will first enter your name and then you can use the GUI to send and receive messages and files. Here are some key features:
 
 - Sending Messages: Type your message in the text entry field at the bottom of the GUI and press the "Send" button to send it.
 - Sending Files: Click the "Attach File" button to select a file from your local machine and send it to the other peer.
-- Receiving Messages and Files: Incoming messages and files will be displayed in the text area at the top of the GUI.
+- Receiving Messages and Files: Incoming messages will be displayed in the text area at the top of the GUI. Incoming files will be saved with the name Received1/2FileName.extension.
 
-# Using the Application (Phase3):
+# Using the Application Terminal (Phase3):
 Once the application is running, you will first enter your name and then you can use the terminal to send and receive messages and files. Here are some key features:
 
 - Sending Messages: Type your message in the terminal and press the "Enter" key to send it.
@@ -38,17 +65,5 @@ The project is organized into the following directories and files:
 - `Host2.py`: The Python script for the second host.
 - `README.md`: The project's main documentation file.
 
-# Specifications:
-The project was developed using Python 3.10 and the following libraries:
-
-- `socket`: Used to create the network connection between the two hosts.
-- `tkinter`: Used to create the graphical user interface (GUI) for the chatting application.
-- `threading`: Used to create multiple threads for sending and receiving messages and files.
-- `os`: Used to interact with the operating system and send files.
-- `time`: Used to add delays in the sending and receiving of messages and files.
-
 # Authors:
-This project was developed by a team of students at the American University of Beirut in the EECE350 course. The team members are:
-Bahaa Ammoury,
-Roni Bou Saab, and
-Samah Ghoussaimy
+Roni Bou Saab, Bahaa Ammoury, and Samah Ghoussaimy
